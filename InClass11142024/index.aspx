38c8b49e75f2264194a3b6b45dbc5ccd11d94810
yemaneag <abelyemaneab@gmail.com>
2024-11-14 10:38:19 -0500

    Add project files.

---
+++ InClass11142024/index.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="InClass11142024.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox ID="lbPizzaCompanies" runat="server" ></asp:ListBox>
+            <br />
+            <asp:Button ID="cmdSelectPizzaCompany" runat="server" Text="Select a Pizza Company" OnClick="cmdSelectPizzaCompany_Click"/>
+            <br />
+            <asp:Label ID="lblSelectedPizzaCompany" runat="server" Text=""></asp:Label>
+            <br />
+            <asp:CheckBoxList ID="cblIngredients" runat="server" />
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ InClass11142024/index.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InClass11142024
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) // Only run this code if its not a post back
+            {
+                //This method runs when the page is loaded into
+                // add a pizza company to the list box
+                // How can we run this only one time?
+                lbPizzaCompanies.Items.Add("LaRosa's");
+                lbPizzaCompanies.Items.Add("Macs");
+                lbPizzaCompanies.Items.Add("Jets Pizza");
+                lbPizzaCompanies.Items.Add("Little Ceasers");
+
+                cblIngredients.Items.Add("Mushrooms");
+                cblIngredients.Items.Add("Bacon");
+                cblIngredients.Items.Add("Onions");
+                cblIngredients.Items.Add("Banana Peppers");
+            }
+
+        }
+
+        protected void cmdSelectPizzaCompany_Click(object sender, EventArgs e)
+        {
+            //This code runs when the cmdSelectPizzaCompany button is pressed
+            //we call this an "event handler"
+            //write the currently selected pizza companies name on the label
+            lblSelectedPizzaCompany.Text = lbPizzaCompanies.Text;
+        }
+    }
+}
